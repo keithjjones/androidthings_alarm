cd52eee19e75699bee977ecdfc37c6d4edaf8127
Keith J. Jones <keith@keithjjones.com>
2017-06-03 22:20:03 -0400

    Reads button presses.

--- app/src/main/java/com/example/androidthings/alarm/MainActivity.java
+++ app/src/main/java/com/example/androidthings/alarm/MainActivity.java
@@ -20,7 +20,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
 
 /**
  * Skeleton of the main Android Things activity. Implement your device's logic
@@ -41,8 +44,12 @@ import com.google.android.things.pio.PeripheralManagerService;
  * is available.
  *
  */
+/* Adapted from https://developer.android.com/things/training/first-device/peripherals.html */
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String BUTTON_PIN_NAME = "BCM6";
+
+    private Gpio mButtonGpio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +57,45 @@ public class MainActivity extends Activity {
         Log.d(TAG, "onCreate");
         PeripheralManagerService service = new PeripheralManagerService();
         Log.d(TAG, "Available GPIO: " + service.getGpioList());
+
+        try {
+            // Step 1. Create GPIO connection.
+            mButtonGpio = service.openGpio(BUTTON_PIN_NAME);
+            // Step 2. Configure as an input.
+            mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+            // Step 3. Enable edge trigger events.
+            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+            // Step 4. Register an event callback.
+            mButtonGpio.registerGpioCallback(mCallback);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
     }
+
+    // Step 4. Register an event callback.
+    private GpioCallback mCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            Log.i(TAG, "GPIO changed, button pressed");
+
+            // Step 5. Return true to keep callback active.
+            return true;
+        }
+    };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+
+        // Step 6. Close the resource
+        if (mButtonGpio != null) {
+            mButtonGpio.unregisterGpioCallback(mCallback);
+            try {
+                mButtonGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
     }
 }
